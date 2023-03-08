ac9ac5b4a79f2454069d1f697b932c95c4fec3d8
jvillad <jyvillad@gmail.com>
2023-03-08 22:14:31 +0900

    Dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,15 @@
-import React from 'react';
+import Panel from '@/components/Panel';
+import '@/styles/global.css';
 
-export default function layout() {
-  return <div>dashboard layout</div>;
+export default function DashboardLayout({ children }) {
+  return (
+    <html lang="en">
+      <head />
+      <body className="h-screen w-screen rainbow-mesh p-6">
+        <Panel className="w-full h-full flex items-center justify-center">
+          {children}
+        </Panel>
+      </body>
+    </html>
+  );
 }
