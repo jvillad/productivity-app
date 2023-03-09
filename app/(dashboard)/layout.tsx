7bd5c5b370f5a0934c52452cb30f3a461756096b
jvillad <jyvillad@gmail.com>
2023-03-09 23:47:25 +0900

    link sidebar component to layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import Panel from '@/components/Panel';
 import '@/styles/global.css';
+import Sidebar from '@/components/Sidebar';
 
 export default function DashboardLayout({ children }) {
   return (
@@ -7,6 +8,7 @@ export default function DashboardLayout({ children }) {
       <head />
       <body className="h-screen w-screen rainbow-mesh p-6">
         <Panel className="w-full h-full flex items-center justify-center">
+          <Sidebar />
           {children}
         </Panel>
       </body>
