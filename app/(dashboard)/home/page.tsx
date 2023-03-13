baf3a134b0afedecd3b6f4f0914eb4439484d87f
jvillad <jyvillad@gmail.com>
2023-03-13 23:07:38 +0900

    added greeting loading component to dashboard

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,3 +1,5 @@
+import GreetingsSkeleton from '@/components/GreetingsSkeleton';
+import { Suspense } from 'react';
 import Greeting from '../../../components/Greeting';
 
 export default async function Home() {
@@ -5,7 +7,9 @@ export default async function Home() {
     <div className="h-full overflow-y-auto pr-6 w-1/1">
       <div className=" h-full  items-stretch justify-center min-h-[content]">
         <div className="flex-1 grow flex">
-          <Greeting />
+          <Suspense fallback={<GreetingsSkeleton />}>
+            <Greeting />
+          </Suspense>
         </div>
         <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
           {/** projects map here */}
